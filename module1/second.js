713de8c3b138036d8b4ae78ab2ffb020f3c32826
Nyamedi Gideon Tetteh <nyamedigideon1@gmail.com>
2026-04-28 19:17:56 +0000

    The study Hub

---
+++ module1/second.js
@@ -0,0 +1,51 @@
+//  Checking out the use of branching in git 
+/*The differences between the (==) and (===) operators in JavaScript are as follows:
+(==) it compares the values only while (===) copares the values and the data type .
+var age = 45
+if( age >= 65){
+console.log("You get your income from your pension")
+}
+else if(age <65 && age >= 18){
+    console.log("Each month you get a salary")
+}
+
+else if(age < 18){
+    console.log("You get an allowance")
+}
+else{
+    console.log("The value of the age variable is not numerical")
+}   
+
+
+set count 
+codition 
+action
+
+
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+};
+console.log('Countdown finished!');
+
+
+function myName(a,b){
+  
+
+    var c = a + b 
+    console.log(c)
+}
+myName(4,5)
+*/
+
+var food = {
+    beans : 10,
+    kelewele : 20,
+    rice :20
+}
+console.log(food)
+
+
+food.banku = "I am banku";      is all about add property to the js file 
+
+
+console.log(food)
